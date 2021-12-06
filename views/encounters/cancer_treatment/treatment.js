28cf1261a50848ff57d5e323e493b4543ee2984a
Martin Suleman <sulemartin87@gmail.com>
2021-12-06 11:52:52 +0200

    fixed outcome state

--- views/encounters/cancer_treatment/treatment.js
+++ views/encounters/cancer_treatment/treatment.js
@@ -96,7 +96,7 @@ function submitEnc(){
 function  updateOutcome(){
   let outcome = {
     location_id: locations[sessionStorage.currentLocation],
-    state: fetched_states['Pre CxCa treatment'],
+    state: fetched_states['Continue follow-up'],
     date: sessionStorage.sessionDate
   }
   let state = JSON.stringify(outcome);
